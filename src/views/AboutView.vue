9f7e758d32fa1b729724a0f070ad2d9714cc09fe
Matias Almada <matias.gabriel.almada@hotmail.com>
2023-03-14 22:34:16 -0300

    modify4

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -29,18 +29,18 @@ onMounted(() => {
     
       <!-- Left Side -->
       
-      <div  class="flex flex-col items-center mb-32 space-y-12 md:w-1/2 h-screen md:my-auto pt-10 md:pt-56 transition-all">
+      <div  class="flex flex-col items-center mb-32 space-y-10 md:w-1/2 h-screen md:my-auto pt-14 md:pt-56 transition-all">
         <h1 class="max-w-md text-center font-bold text-4xl md:text-6xl   md:text-left text-curriculum-primary dark:text-violet-500"> Sobre Mi</h1>
         <div class="space-y-8" ref="text">
-        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-xl dark:font-bold">
+        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-base dark:font-bold">
           Soy un desarrollador web enfocado a la parte del Front-end viviendo en Buenos Aires, Argentina.
         </p>
-        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-xl dark:font-bold">
+        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-base dark:font-bold">
           
           Con 26 años he completado una tecnicatura en analisis, desarrollo y programacion de aplicaciones iniciandome asi en el mundo de la programacion.
           
         </p>
-        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-xl dark:font-bold">
+        <p class="max-w-sm text-center text-gray-500 md:text-left dark:text-gray-300 text-base dark:font-bold">
           Actualmente me encuentro terminando una licenciatura en gestion de la tecnologia en la universidad nacional de la matanza. 
         </p>
       </div>
